7864083f3aee2d0fe76dd7edd0006aba5203d337
alejocram <jcarmon4@eafit.edu.co>
2019-03-09 15:22:04 -0500

    Se corrige el Report Model

--- report/models.py
+++ report/models.py
@@ -6,7 +6,7 @@ class Report(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     car_plate = models.CharField(verbose_name='Placa', max_length=6)
     type = models.TextField(verbose_name='Tipo')
-    score = models.CharField(verbose='Calificación')
+    score = models.CharField(verbose_name='Calificación', max_length=1)
     comment = models.TextField(verbose_name='Comentario')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
