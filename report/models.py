a74b65c65d531a505f4200cc24c8349ca9e9bd2d
alejocram <jcarmon4@eafit.edu.co>
2019-03-09 12:54:47 -0500

    Se crea el modelo y se importa el uuid para indicar el primary key

--- report/models.py
+++ report/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Report(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    car_plate = models.CharField(verbose_name='Placa', max_length=6)
+    type = models.TextField(verbose_name='Tipo')
+    score = models.CharField(verbose='Calificación')
+    comment = models.TextField(verbose_name='Comentario')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
